d2ea18705572e4fc0f9e33333b18eda1c2f1ab52
videoben74-hub <videoben74@gmail.com>
2026-09-22 23:58:40 -0400

    Invoke onProfilePhotoChange after setting photo

--- App.js
+++ App.js
@@ -1978,7 +1978,9 @@ const isRbqValid = /^\d{4}-\d{4}-\d{2}$/.test(rbq.trim());
 
     setClientPhoto(publicUrl);
     setProfilePhoto(publicUrl);
-
+if (onProfilePhotoChange) {
+  onProfilePhotoChange(publicUrl);
+}
   } catch (error) {
     console.log('Erreur photo client :', error);
     alert(error.message);
